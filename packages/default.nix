a9e5f89a1ab1b4c389c29f3de351da62a9e3e965
Jordan Williams <jordan@jwillikers.com>
2024-10-24 14:29:40 -0500

    Remove comment

--- packages/default.nix
+++ packages/default.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }:
 {
-  # Nushell's hashing functions used in update-nix-direnv are only available in new versions.
   update-nix-direnv = pkgs.callPackage ./update-nix-direnv/package.nix { };
   update-nixos-release = pkgs.callPackage ./update-nixos-release/package.nix { };
 }
